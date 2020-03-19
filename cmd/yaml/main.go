072f4b7783e8a74cde4275ef7da179c315a59afa
Kévin Gomez <contact@kevingomez.fr>
2020-03-19 23:25:12 +0100

    End to end example

--- cmd/yaml/main.go
+++ cmd/yaml/main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 
 	"github.com/K-Phoen/grabana"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Fprint(os.Stderr, "Usage: go run -mod=vendor main.go file\n")
+	if len(os.Args) != 4 {
+		fmt.Fprint(os.Stderr, "Usage: go run -mod=vendor main.go file http://grafana-host:3000 api-key\n")
 		os.Exit(1)
 	}
 
@@ -21,12 +23,38 @@ func main() {
 		os.Exit(1)
 	}
 
-	builder, err := grabana.UnmarshalYAML(bytes.NewBuffer(content))
+	dashboard, err := grabana.UnmarshalYAML(bytes.NewBuffer(content))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Could not parse file: %s\n", err)
 		os.Exit(1)
 	}
 
-	json, _ := builder.MarshalJSON()
+	json, _ := dashboard.MarshalJSON()
 	fmt.Printf("Dashboard JSON: %s\n", json)
+
+	ctx := context.Background()
+	client := grabana.NewClient(&http.Client{}, os.Args[2], os.Args[3])
+
+	// create the folder holding the dashboard for the service
+	folder, err := client.GetFolderByTitle(ctx, "Test Folder")
+	if err != nil && err != grabana.ErrFolderNotFound {
+		fmt.Printf("Could not create folder: %s\n", err)
+		os.Exit(1)
+	}
+	if folder == nil {
+		folder, err = client.CreateFolder(ctx, "Test Folder")
+		if err != nil {
+			fmt.Printf("Could not create folder: %s\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("Folder created (id: %d, uid: %s)\n", folder.ID, folder.UID)
+	}
+
+	if _, err := client.UpsertDashboard(ctx, folder, dashboard); err != nil {
+		fmt.Printf("Could not create dashboard: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("The deed is done.")
 }
