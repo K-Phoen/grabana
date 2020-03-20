68b3b4ffa06f53200364a6193dff76ce6c96da1d
Kévin Gomez <kevin.gomez@voiapp.io>
2020-03-20 17:03:16 +0100

    Add basic support for singlestat panels

--- yaml.go
+++ yaml.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/K-Phoen/grabana/graph"
 	"github.com/K-Phoen/grabana/row"
+	"github.com/K-Phoen/grabana/singlestat"
 	"github.com/K-Phoen/grabana/table"
 	"github.com/K-Phoen/grabana/target/prometheus"
 	"github.com/K-Phoen/grabana/variable/constant"
@@ -211,8 +212,9 @@ func (r dashboardRow) toOption() (DashboardBuilderOption, error) {
 }
 
 type dashboardPanel struct {
-	Graph *dashboardGraph
-	Table *dashboardTable
+	Graph      *dashboardGraph
+	Table      *dashboardTable
+	SingleStat *dashboardSingleStat `yaml:"single_stat"`
 }
 
 func (panel dashboardPanel) toOption() (row.Option, error) {
@@ -221,6 +223,9 @@ func (panel dashboardPanel) toOption() (row.Option, error) {
 	}
 	if panel.Table != nil {
 		return panel.Table.toOption()
+	}
+	if panel.SingleStat != nil {
+		return panel.SingleStat.toOption()
 	}
 
 	return nil, fmt.Errorf("panel not configured")
@@ -336,6 +341,70 @@ func (tablePanel dashboardTable) toOption() (row.Option, error) {
 func (tablePanel *dashboardTable) target(t target) (table.Option, error) {
 	if t.Prometheus != nil {
 		return table.WithPrometheusTarget(t.Prometheus.Query, t.Prometheus.toOptions()...), nil
+	}
+
+	return nil, fmt.Errorf("target not configured")
+}
+
+type dashboardSingleStat struct {
+	Title      string
+	Span       float32
+	Height     string
+	Datasource string
+	Unit       string
+	Targets    []target
+	Thresholds [2]string
+	Colors     [3]string
+	Color      []string
+}
+
+func (singleStatPanel dashboardSingleStat) toOption() (row.Option, error) {
+	opts := []singlestat.Option{}
+
+	if singleStatPanel.Span != 0 {
+		opts = append(opts, singlestat.Span(singleStatPanel.Span))
+	}
+	if singleStatPanel.Height != "" {
+		opts = append(opts, singlestat.Height(singleStatPanel.Height))
+	}
+	if singleStatPanel.Datasource != "" {
+		opts = append(opts, singlestat.DataSource(singleStatPanel.Datasource))
+	}
+	if singleStatPanel.Unit != "" {
+		opts = append(opts, singlestat.Unit(singleStatPanel.Unit))
+	}
+	if singleStatPanel.Thresholds[0] != "" {
+		opts = append(opts, singlestat.Thresholds(singleStatPanel.Thresholds))
+	}
+	if singleStatPanel.Colors[0] != "" {
+		opts = append(opts, singlestat.Colors(singleStatPanel.Colors))
+	}
+
+	for _, colorTarget := range singleStatPanel.Color {
+		if colorTarget == "value" {
+			opts = append(opts, singlestat.ColorValue())
+		} else if colorTarget == "background" {
+			opts = append(opts, singlestat.ColorBackground())
+		} else {
+			return nil, fmt.Errorf("invalid coloring target '%s'", colorTarget)
+		}
+	}
+
+	for _, t := range singleStatPanel.Targets {
+		opt, err := singleStatPanel.target(t)
+		if err != nil {
+			return nil, err
+		}
+
+		opts = append(opts, opt)
+	}
+
+	return row.WithSingleStat(singleStatPanel.Title, opts...), nil
+}
+
+func (singleStatPanel dashboardSingleStat) target(t target) (singlestat.Option, error) {
+	if t.Prometheus != nil {
+		return singlestat.WithPrometheusTarget(t.Prometheus.Query, t.Prometheus.toOptions()...), nil
 	}
 
 	return nil, fmt.Errorf("target not configured")
