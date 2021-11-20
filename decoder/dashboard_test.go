d322e473d3ffea69c81c01e8039e20f54f3762f7
Kévin Gomez <contact@kevingomez.fr>
2021-11-20 01:46:14 +0100

    Test timeseries decoding from dashboard

--- decoder/dashboard_test.go
+++ decoder/dashboard_test.go
@@ -32,6 +32,7 @@ func TestUnmarshalYAML(t *testing.T) {
 		heatmapPanel(),
 		graphPanelWithGraphiteTarget(),
 		graphPanelWithInfluxdbTarget(),
+		timeseriesPanel(),
 		collapseRow(),
 	}
 
@@ -1870,6 +1871,136 @@ rows:
 	}
 }
 
+func timeseriesPanel() testCase {
+	yaml := `title: Awesome dashboard
+
+rows:
+  - name: Test row
+    panels:
+    - timeseries:
+        title: Total Request per Second
+        description: Does it perform?
+        span: 12
+        targets:
+        - prometheus:
+            query: "go_memstats_heap_alloc_bytes"
+            ref: A
+`
+	json := `{
+	"slug": "",
+	"title": "Awesome dashboard",
+	"originalTitle": "",
+	"tags": null,
+	"style": "dark",
+	"timezone": "",
+	"editable": false,
+	"hideControls": false,
+	"sharedCrosshair": false,
+	"templating": {"list": null},
+	"annotations": {"list": null},
+	"links": null,
+	"panels": null,
+	"rows": [
+		{
+			"title": "Test row",
+			"collapse": false,
+			"editable": true,
+			"height": "250px",
+			"repeat": null,
+			"showTitle": true,
+			"panels": [
+				{
+					"editable": false,
+					"error": false,
+					"gridPos": {},
+					"id": 10,
+					"fieldConfig": {
+						"defaults": {
+							"unit": "",
+							"color": {
+								"fixedColor": "green",
+								"seriesBy": "last",
+								"mode": "palette-classic"
+							},
+							"thresholds": {
+								"mode": "",
+								"steps": null
+							},
+							"custom": {
+								"axisPlacement": "auto",
+								"barAlignment": 0,
+								"drawStyle": "line",
+								"fillOpacity": 25,
+								"gradientMode": "opacity",
+								"hideFrom": {
+									"legend": false,
+									"tooltip": false,
+									"viz": false
+								},
+								"lineInterpolation": "linear",
+								"lineStyle": {
+									"fill": "solid"
+								},
+								"lineWidth": 1,
+								"pointSize": 5,
+								"scaleDistribution": {
+									"type": "linear"
+								},
+								"showPoints": "",
+								"spanNulls": false,
+								"stacking": {
+									"group": "",
+									"mode": ""
+								},
+								"thresholdsStyle": {
+									"mode": ""
+								}
+							}
+						}
+					},
+					"isNew": false,
+					"span": 12,
+					"title": "Total Request per Second",
+					"description": "Does it perform?",
+					"transparent": false,
+					"type": "timeseries",
+					"targets": [
+						{
+							"refId": "A",
+							"expr": "go_memstats_heap_alloc_bytes",
+							"format": "time_series"
+						}
+					],
+					"options": {
+						"legend": {
+							"calcs": [],
+							"placement": "bottom",
+							"displayMode": "list"
+						},
+						"tooltip": {
+							"mode": "single"
+						}
+					}
+				}
+			]
+		}
+	],
+	"time": {"from": "now-3h", "to": "now"},
+	"timepicker": {
+		"refresh_intervals": ["5s","10s","30s","1m","5m","15m","30m","1h","2h","1d"],
+		"time_options": ["5m","15m","1h","6h","12h","24h","2d","7d","30d"]
+	},
+	"schemaVersion": 0,
+	"version": 0
+}`
+
+	return testCase{
+		name:                "single row with timeseries panel",
+		yaml:                yaml,
+		expectedGrafanaJSON: json,
+	}
+}
+
 func collapseRow() testCase {
 	yaml := `title: Awesome dashboard
 
@@ -1915,7 +2046,7 @@ rows:
 					"editable": false,
 					"error": false,
 					"gridPos": {},
-					"id": 10,
+					"id": 11,
 					"isNew": false,
 					"pageSize": 0,
 					"scroll": false,
