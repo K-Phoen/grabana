b6abc1c3c09d52d0a00de5b058fe10dd9cc41fae
Kévin Gomez <contact@kevingomez.fr>
2022-05-01 22:21:33 +0200

    Test alert collectin from timeseries and graphs

--- row/row_test.go
+++ row/row_test.go
@@ -3,6 +3,9 @@ package row
 import (
 	"testing"
 
+	"github.com/K-Phoen/grabana/alert"
+	"github.com/K-Phoen/grabana/graph"
+	"github.com/K-Phoen/grabana/timeseries"
 	"github.com/K-Phoen/sdk"
 	"github.com/stretchr/testify/require"
 )
@@ -44,6 +47,33 @@ func TestRowsCanHaveGraphs(t *testing.T) {
 	req.Len(panel.builder.Panels, 1)
 }
 
+func TestRowsCanHaveGraphsAndAlert(t *testing.T) {
+	req := require.New(t)
+	board := sdk.NewBoard("")
+
+	panel := New(
+		board,
+		"",
+		WithGraph(
+			"HTTP Rate",
+			graph.DataSource("Prometheus"),
+			graph.Alert(
+				"Too many heap allocations",
+				alert.WithPrometheusQuery(
+					"A",
+					"sum(go_memstats_heap_alloc_bytes{app!=\"\"}) by (app)",
+				),
+				alert.If(alert.Avg, "A", alert.IsAbove(3)),
+			),
+		),
+	)
+
+	req.Len(panel.builder.Panels, 1)
+	req.Len(panel.Alerts(), 1)
+
+	req.Equal("Prometheus", panel.Alerts()[0].Datasource)
+}
+
 func TestRowsCanHaveTimeSeries(t *testing.T) {
 	req := require.New(t)
 	board := sdk.NewBoard("")
@@ -51,6 +81,33 @@ func TestRowsCanHaveTimeSeries(t *testing.T) {
 	panel := New(board, "", WithTimeSeries("HTTP Rate"))
 
 	req.Len(panel.builder.Panels, 1)
+}
+
+func TestRowsCanHaveTimeSeriesAndAlert(t *testing.T) {
+	req := require.New(t)
+	board := sdk.NewBoard("")
+
+	panel := New(
+		board,
+		"",
+		WithTimeSeries(
+			"HTTP Rate",
+			timeseries.DataSource("Prometheus"),
+			timeseries.Alert(
+				"Too many heap allocations",
+				alert.WithPrometheusQuery(
+					"A",
+					"sum(go_memstats_heap_alloc_bytes{app!=\"\"}) by (app)",
+				),
+				alert.If(alert.Avg, "A", alert.IsAbove(3)),
+			),
+		),
+	)
+
+	req.Len(panel.builder.Panels, 1)
+	req.Len(panel.Alerts(), 1)
+
+	req.Equal("Prometheus", panel.Alerts()[0].Datasource)
 }
 
 func TestRowsCanHaveTextPanels(t *testing.T) {
