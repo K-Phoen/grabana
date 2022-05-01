3055061a0d65bd6ea4a4cdcd8831bdffcd3a1f3b
Kévin Gomez <contact@kevingomez.fr>
2022-05-01 23:43:05 +0200

    Add some tests for stackdriver alert queries decoding

--- decoder/alert_targets_test.go
+++ decoder/alert_targets_test.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	alertBuilder "github.com/K-Phoen/grabana/alert"
+	"github.com/K-Phoen/grabana/alert/queries/stackdriver"
 	"github.com/stretchr/testify/require"
 )
 
@@ -207,4 +208,392 @@ func TestDecodingAStackdriverTargetWithInvalidLookback(t *testing.T) {
 	_, err := target.toOption()
 
 	req.ErrorIs(err, ErrInvalidLookback)
+}
+
+func TestDecodingAStackdriverTargetWithInvalidType(t *testing.T) {
+	req := require.New(t)
+
+	target := AlertTarget{
+		Stackdriver: &AlertStackdriver{
+			Ref:  "A",
+			Type: "invalid",
+		},
+	}
+
+	_, err := target.toOption()
+
+	req.ErrorIs(err, ErrInvalidStackdriverType)
+}
+
+func TestDecodingStackdriverTargetWithValidTypes(t *testing.T) {
+	validTypes := []string{"delta", "gauge", "cumulative"}
+
+	for _, inputType := range validTypes {
+		t.Run(inputType, func(t *testing.T) {
+			req := require.New(t)
+
+			target := AlertTarget{
+				Stackdriver: &AlertStackdriver{
+					Ref:  "A",
+					Type: inputType,
+				},
+			}
+
+			_, err := target.toOption()
+
+			req.NoError(err)
+		})
+	}
+}
+
+func TestDecodingStackdriverTarget(t *testing.T) {
+	req := require.New(t)
+
+	target := AlertTarget{
+		Stackdriver: &AlertStackdriver{
+			Ref:         "A",
+			Lookback:    "15m",
+			Type:        "gauge",
+			Metric:      "cloudsql.googleapis.com/database/cpu/utilization",
+			GroupBy:     []string{"resource.label.database_id"},
+			Aggregation: "mean",
+			Alignment: &StackdriverAlertAlignment{
+				Method: "mean",
+				Period: "stackdriver-auto",
+			},
+		},
+	}
+
+	opt, err := target.toOption()
+
+	req.NoError(err, ErrInvalidStackdriverType)
+
+	alert := alertBuilder.New("", opt)
+
+	req.Len(alert.Builder.Rules, 1)
+	req.Len(alert.Builder.Rules[0].GrafanaAlert.Data, 1)
+
+	query := alert.Builder.Rules[0].GrafanaAlert.Data[0]
+	stackdriverData := query.Model.MetricQuery
+
+	req.Equal("A", query.RefID)
+	req.Equal("A", query.Model.RefID)
+	req.Equal("GAUGE", stackdriverData.MetricKind)
+	req.Equal("cloudsql.googleapis.com/database/cpu/utilization", stackdriverData.MetricType)
+	req.ElementsMatch([]string{"resource.label.database_id"}, stackdriverData.GroupBys)
+	req.Equal("REDUCE_MEAN", stackdriverData.CrossSeriesReducer)
+	req.Equal("ALIGN_MEAN", stackdriverData.PerSeriesAligner)
+	req.Equal("stackdriver-auto", stackdriverData.AlignmentPeriod)
+	req.Equal(int((15 * time.Minute).Seconds()), query.RelativeTimeRange.From)
+}
+
+func TestDecodingStackdriverPreprocessor(t *testing.T) {
+	testCases := []struct {
+		input         string
+		expected      string
+		expectedError error
+	}{
+		{
+			input:         "delta",
+			expected:      "delta",
+			expectedError: nil,
+		},
+		{
+			input:         "rate",
+			expected:      "rate",
+			expectedError: nil,
+		},
+		{
+			input:         "invalid",
+			expected:      "",
+			expectedError: ErrInvalidStackdriverPreprocessor,
+		},
+	}
+
+	for _, testCase := range testCases {
+		tc := testCase
+
+		t.Run(tc.input, func(t *testing.T) {
+			req := require.New(t)
+
+			target := AlertTarget{
+				Stackdriver: &AlertStackdriver{
+					Ref:          "A",
+					Type:         "delta",
+					Preprocessor: tc.input,
+				},
+			}
+
+			opt, err := target.toOption()
+
+			if tc.expectedError != nil {
+				req.ErrorIs(tc.expectedError, err)
+				return
+			} else {
+				req.NoError(err)
+			}
+
+			alert := alertBuilder.New("", opt)
+			query := alert.Builder.Rules[0].GrafanaAlert.Data[0]
+
+			req.Equal(tc.expected, query.Model.MetricQuery.Preprocessor)
+		})
+	}
+}
+
+func TestDecodingStackdriverAggregation(t *testing.T) {
+	testCases := []struct {
+		input         string
+		expected      stackdriver.Reducer
+		expectedError error
+	}{
+		{
+			input:         "none",
+			expected:      stackdriver.ReduceNone,
+			expectedError: nil,
+		},
+		{
+			input:         "mean",
+			expected:      stackdriver.ReduceMean,
+			expectedError: nil,
+		},
+		{
+			input:         "min",
+			expected:      stackdriver.ReduceMin,
+			expectedError: nil,
+		},
+		{
+			input:         "max",
+			expected:      stackdriver.ReduceMax,
+			expectedError: nil,
+		},
+		{
+			input:         "sum",
+			expected:      stackdriver.ReduceSum,
+			expectedError: nil,
+		},
+		{
+			input:         "stddev",
+			expected:      stackdriver.ReduceStdDev,
+			expectedError: nil,
+		},
+		{
+			input:         "count",
+			expected:      stackdriver.ReduceCount,
+			expectedError: nil,
+		},
+		{
+			input:         "count_true",
+			expected:      stackdriver.ReduceCountTrue,
+			expectedError: nil,
+		},
+		{
+			input:         "count_false",
+			expected:      stackdriver.ReduceCountFalse,
+			expectedError: nil,
+		},
+		{
+			input:         "fraction_true",
+			expected:      stackdriver.ReduceCountFractionTrue,
+			expectedError: nil,
+		},
+		{
+			input:         "percentile_99",
+			expected:      stackdriver.ReducePercentile99,
+			expectedError: nil,
+		},
+		{
+			input:         "percentile_95",
+			expected:      stackdriver.ReducePercentile95,
+			expectedError: nil,
+		},
+		{
+			input:         "percentile_50",
+			expected:      stackdriver.ReducePercentile50,
+			expectedError: nil,
+		},
+		{
+			input:         "percentile_05",
+			expected:      stackdriver.ReducePercentile05,
+			expectedError: nil,
+		},
+		{
+			input:         "invalid",
+			expected:      "",
+			expectedError: ErrInvalidStackdriverAggregation,
+		},
+	}
+
+	for _, testCase := range testCases {
+		tc := testCase
+
+		t.Run(tc.input, func(t *testing.T) {
+			req := require.New(t)
+
+			target := AlertTarget{
+				Stackdriver: &AlertStackdriver{
+					Ref:         "A",
+					Type:        "delta",
+					Aggregation: tc.input,
+				},
+			}
+
+			opt, err := target.toOption()
+
+			if tc.expectedError != nil {
+				req.ErrorIs(tc.expectedError, err)
+				return
+			} else {
+				req.NoError(err)
+			}
+
+			alert := alertBuilder.New("", opt)
+			query := alert.Builder.Rules[0].GrafanaAlert.Data[0]
+
+			req.Equal(string(tc.expected), query.Model.MetricQuery.CrossSeriesReducer)
+		})
+	}
+}
+
+func TestDecodingStackdriverAlignment(t *testing.T) {
+	testCases := []struct {
+		input         string
+		expected      stackdriver.Aligner
+		expectedError error
+	}{
+		{
+			input:         "none",
+			expected:      stackdriver.AlignNone,
+			expectedError: nil,
+		},
+		{
+			input:         "delta",
+			expected:      stackdriver.AlignDelta,
+			expectedError: nil,
+		},
+		{
+			input:         "rate",
+			expected:      stackdriver.AlignRate,
+			expectedError: nil,
+		},
+		{
+			input:         "interpolate",
+			expected:      stackdriver.AlignInterpolate,
+			expectedError: nil,
+		},
+		{
+			input:         "next_older",
+			expected:      stackdriver.AlignNextOlder,
+			expectedError: nil,
+		},
+		{
+			input:         "min",
+			expected:      stackdriver.AlignMin,
+			expectedError: nil,
+		},
+		{
+			input:         "max",
+			expected:      stackdriver.AlignMax,
+			expectedError: nil,
+		},
+		{
+			input:         "mean",
+			expected:      stackdriver.AlignMean,
+			expectedError: nil,
+		},
+		{
+			input:         "count",
+			expected:      stackdriver.AlignCount,
+			expectedError: nil,
+		},
+		{
+			input:         "sum",
+			expected:      stackdriver.AlignSum,
+			expectedError: nil,
+		},
+		{
+			input:         "stddev",
+			expected:      stackdriver.AlignStdDev,
+			expectedError: nil,
+		},
+		{
+			input:         "count_true",
+			expected:      stackdriver.AlignCountTrue,
+			expectedError: nil,
+		},
+		{
+			input:         "count_false",
+			expected:      stackdriver.AlignCountFalse,
+			expectedError: nil,
+		},
+		{
+			input:         "fraction_true",
+			expected:      stackdriver.AlignFractionTrue,
+			expectedError: nil,
+		},
+		{
+			input:         "percentile_99",
+			expected:      stackdriver.AlignPercentile99,
+			expectedError: nil,
+		},
+		{
+			input:         "percentile_95",
+			expected:      stackdriver.AlignPercentile95,
+			expectedError: nil,
+		},
+		{
+			input:         "percentile_50",
+			expected:      stackdriver.AlignPercentile50,
+			expectedError: nil,
+		},
+		{
+			input:         "percentile_05",
+			expected:      stackdriver.AlignPercentile05,
+			expectedError: nil,
+		},
+		{
+			input:         "percent_change",
+			expected:      stackdriver.AlignPercentChange,
+			expectedError: nil,
+		},
+		{
+			input:         "invalid",
+			expected:      "",
+			expectedError: ErrInvalidStackdriverAlignment,
+		},
+	}
+
+	for _, testCase := range testCases {
+		tc := testCase
+
+		t.Run(tc.input, func(t *testing.T) {
+			req := require.New(t)
+
+			target := AlertTarget{
+				Stackdriver: &AlertStackdriver{
+					Ref:  "A",
+					Type: "delta",
+					Alignment: &StackdriverAlertAlignment{
+						Method: tc.input,
+						Period: "stackdriver-auto",
+					},
+				},
+			}
+
+			opt, err := target.toOption()
+
+			if tc.expectedError != nil {
+				req.ErrorIs(tc.expectedError, err)
+				return
+			} else {
+				req.NoError(err)
+			}
+
+			alert := alertBuilder.New("", opt)
+			query := alert.Builder.Rules[0].GrafanaAlert.Data[0]
+
+			req.Equal(string(tc.expected), query.Model.MetricQuery.PerSeriesAligner)
+			req.Equal("stackdriver-auto", query.Model.MetricQuery.AlignmentPeriod)
+		})
+	}
 }
