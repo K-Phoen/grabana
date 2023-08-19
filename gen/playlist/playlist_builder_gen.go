3839a9d914a0aac21f704128a75d310c83b348ec
Kévin Gomez <contact@kevingomez.fr>
2023-08-19 17:37:52 +0200

    regen some stuff

--- gen/playlist/playlist_builder_gen.go
+++ gen/playlist/playlist_builder_gen.go
@@ -1,45 +1,18 @@
 package playlist
 
-import "encoding/json"
-
 type Option func(builder *Builder) error
 
 type Builder struct {
-	internal *Dashboard
+	internal *Playlist
 }
 
-func New(title string, options ...Option) (Builder, error) {
-	dashboard := &Dashboard{
-		Title: title,
+func Name(name string) Option {
+	return func(builder *Builder) error {
+
+		builder.internal.Name = name
+
+		return nil
 	}
-
-	builder := &Builder{internal: dashboard}
-
-	for _, opt := range options {
-		if err := opt(builder); err != nil {
-			return *builder, err
-		}
-	}
-
-	return *builder, nil
-}
-
-// MarshalJSON implements the encoding/json.Marshaler interface.
-//
-// This method can be used to render the dashboard as JSON
-// which your configuration management tool of choice can then feed into
-// Grafana's dashboard via its provisioning support.
-// See https://grafana.com/docs/grafana/latest/administration/provisioning/#dashboards
-func (builder *Builder) MarshalJSON() ([]byte, error) {
-	return json.Marshal(builder.internal)
-}
-
-// MarshalIndentJSON renders the dashboard as indented JSON
-// which your configuration management tool of choice can then feed into
-// Grafana's dashboard via its provisioning support.
-// See https://grafana.com/docs/grafana/latest/administration/provisioning/#dashboards
-func (builder *Builder) MarshalIndentJSON() ([]byte, error) {
-	return json.MarshalIndent(builder.internal, "", "  ")
 }
 
 func Interval(interval string) Option {
@@ -55,15 +28,6 @@ func Items(items []PlaylistItem) Option {
 	return func(builder *Builder) error {
 
 		builder.internal.Items = items
-
-		return nil
-	}
-}
-
-func Name(name string) Option {
-	return func(builder *Builder) error {
-
-		builder.internal.Name = name
 
 		return nil
 	}
