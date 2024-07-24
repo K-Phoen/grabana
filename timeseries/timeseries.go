571c83335ee757234ac1688b67488368bedffba2
Valery Bitsoev <valerybitsoev@nebius.com>
2024-07-24 17:26:38 +0200

    Add an option to connect nulls to the timeseries

--- timeseries/timeseries.go
+++ timeseries/timeseries.go
@@ -450,3 +450,11 @@ func FieldOverride(m fields.Matcher, opts ...fields.OverrideOption) Option {
 		return nil
 	}
 }
+
+func ConnectNulls(enabled bool) Option {
+	return func(timeseries *TimeSeries) error {
+		timeseries.Builder.TimeseriesPanel.FieldConfig.Defaults.Custom.SpanNulls = enabled
+
+		return nil
+	}
+}
